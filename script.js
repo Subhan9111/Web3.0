4aff1f04e50c6c55c6c06dc2515d67bd2b1dbee4
Subhan9111 <monea007@osloskolen.no>
2026-03-05 11:08:45 +0100

    e
    w

--- script.js
+++
@@ -1,9 +0,0 @@
-const scrollBtn = document.getElementById("scrollBtn");
-
-scrollBtn.addEventListener("click", function () {
-  const cvSection = document.getElementById("CV");
-
-  cvSection.scrollIntoView({
-    behavior: "smooth",
-  });
-});
